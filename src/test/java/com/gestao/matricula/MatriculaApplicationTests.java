7d606222ee65e53c94c23271436924ec310b55d9
bispobr <156684382+bispobr@users.noreply.github.com>
2026-07-19 16:13:19 -0300

    feat: add classe principal

---
+++ src/test/java/com/gestao/matricula/MatriculaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gestao.matricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
